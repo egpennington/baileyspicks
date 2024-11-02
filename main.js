89bf39e0c4c1833974f4e9a9f7d784c34df0ff7c
egpennington <egpennington@hotmail.com>
2024-11-02 12:43:35 -0800

    added nav, search and cards

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Bailey, fetch!")
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const searchName = document.getElementById("search-name").value;
+    console.log("Search term:", searchName); 
+});
