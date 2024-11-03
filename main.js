95b82136480feb217ef0628cf63fb0853d506d08
egpennington <egpennington@hotmail.com>
2024-11-02 19:10:38 -0800

    added js fetch and data to index

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 // 288650c8
 const apiKey = `288650c8`
-const title = 'start-up';
+let fetchTitle = '';
 console.log("Bailey, fetch!")
 
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
-    const searchName = document.getElementById("search-name").value;
-    console.log("Search term:", searchName);
+    fetchTitle = document.getElementById("search-name").value;
+    console.log("Search term:", fetchTitle);
+    getMovieData(fetchTitle)
+    this.reset()
 });
 
-function getMovieData() {
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`)
+function getMovieData(title) {
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
         .then(res => res.json())
         .then(data => {
-            
-            console.log(data.Poster)
-            console.log(data.Plot)
+            data.Search.forEach(movie => {
+                document.getElementById("cards-display").innerHTML += `
+                    <div class="card-info">
+                        <img class="moviePoster" src="${movie.Poster}" alt="movie poster">
+                        <div class="movie-detail">
+                            <div class="movie-detail-top">
+                                <h3 class="title">${movie.Title}</h3>
+                                <div class="rating">
+                                    <p><i class="fa-solid fa-star star"></i> Rating not available here</p>                          
+                                </div>                                               
+                            </div>
+                            <div class="movie-detail-middle">
+                                <p id="year">Year: ${movie.Year}</p>
+                                <p id="type">Type: ${movie.Type}</p>
+                                <p id="add-movie" class="add-movie"><i class="fa-solid fa-circle-plus fa-sm"></i> watchlist</p>
+                            </div>                  
+                        </div>                
+                    </div>
+                `
+            })
         })
 }
-
-function getMoviePoster() {
-    fetch(`http://img.omdbapi.com/?apikey=${apiKey}&t=${title}`)
-        .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-}
-
-// https://www.omdbapi.com/?i=tt3896198&apikey=288650c8
-
-getMovieData()
