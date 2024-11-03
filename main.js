1bf109a1f3a6e87f4c3df56a90e8c17e06cf8a77
egpennington <egpennington@hotmail.com>
2024-11-03 08:52:55 -0900

    fetch s and i with imbd id

--- main.js
+++ main.js
@@ -11,29 +11,32 @@ document.querySelector('form').addEventListener('submit', function(e) {
     this.reset()
 });
 
-function getMovieData(title) {
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
-        .then(res => res.json())
-        .then(data => {
-            data.Search.forEach(movie => {
-                document.getElementById("cards-display").innerHTML += `
-                    <div class="card-info">
-                        <img class="moviePoster" src="${movie.Poster}" alt="movie poster">
-                        <div class="movie-detail">
-                            <div class="movie-detail-top">
-                                <h3 class="title">${movie.Title}</h3>
-                                <div class="rating">
-                                    <p><i class="fa-solid fa-star star"></i> Rating not available here</p>                          
-                                </div>                                               
-                            </div>
-                            <div class="movie-detail-middle">
-                                <p id="year">Year: ${movie.Year}</p>
-                                <p id="type">Type: ${movie.Type}</p>
-                                <p id="add-movie" class="add-movie"><i class="fa-solid fa-circle-plus fa-sm"></i> watchlist</p>
-                            </div>                  
-                        </div>                
+async function getMovieData(title) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
+    const data = await res.json()
+    data.Search.forEach(async (movie) => {
+        const movieDetail = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+        const movieData = await movieDetail.json()
+        console.log(movieData)
+        document.getElementById("cards-display").innerHTML += `
+            <div class="card-info">
+                <img class="moviePoster" src="${movieData.Poster}" alt="movie poster">
+                <div class="movie-detail">
+                    <div class="movie-detail-top">
+                        <h3 class="title">${movieData.Title}</h3>
+                        <div class="rating">
+                        <p><i class="fa-solid fa-star star"></i> ${movieData.Ratings[0].Value}</p>                          
+                    </div>                                               
+                </div>
+                <div class="movie-detail-middle">
+                        <p id="length">${movieData.Runtime}</p>
+                        <p id="genre">${movieData.Genre}</p>
+                        <p id="add-movie" class="add-movie"><i class="fa-solid fa-circle-plus fa-sm"></i> watchlist</p>
                     </div>
-                `
-            })
-        })
+                    <div class="movie-detail-bottom">
+                        <p>${movieData.Plot}</p>
+                    </div>               
+            </div>
+            `
+    })
 }
