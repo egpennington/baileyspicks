797d9e99b1b2c6b3ba59bad9a57cd7753e35f485
egpennington <egpennington@hotmail.com>
2024-11-08 14:17:44 -0900

    changed https to http

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function renderMovie(movieData) {
 
 async function getMovieData(title) {
     try{ 
-        const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
         const data = await res.json()
 
         document.getElementById("cards-display").innerHTML = ""
